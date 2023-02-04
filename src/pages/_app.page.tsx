f22c55cddb84e3bac443cd3a16a50917d9276377
phuongbao90 <phuongbao90@gmail.com>
2023-02-05 00:13:37 +0700

    refractor: react-query provider into different comp

---
+++ src/pages/_app.page.tsx
@@ -0,0 +1,53 @@
+import { ReactElement, ReactNode, useState, useEffect } from "react"
+import "@/styles/globals.css"
+import { RootLayout } from "@/components/common/Layout"
+import { AppProps } from "next/app"
+import { NextPage } from "next"
+import { initMocks } from "@/mocks"
+import { ENABLE_MOCK } from "@/constant"
+import { ReactQueryProvider } from "@/providers"
+
+if (ENABLE_MOCK && process.env.NODE_ENV === "development") {
+	initMocks()
+}
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+	getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+	Component: NextPageWithLayout
+}
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+	const getLayout =
+		Component.getLayout || ((page) => <RootLayout>{page}</RootLayout>)
+
+	// useEffect(() => {
+	// 	async function fetchUserData() {
+	// 		const storedUserId = localStorage.getItem("user_id")
+
+	// 		if (storedUserId && storedUserId !== "undefined") {
+	// 			// storage.setItem("user_id", data.id)
+	// 			// const res = await fetch(`/api/user?user_id=${storedUserId}`)
+	// 			// const data = await res.json()
+	// 			// localStorage.setItem("user_id", data.id)
+	// 		} else {
+	// 			// const res = await fetch(`/api/user`, { method: "POST" })
+	// 			// const createdUser = await res.json()
+	// 			// localStorage.setItem("user_id", createdUser.id)
+	// 			// storage.setItem("user_id", data.id)
+	// 		}
+	// 	}
+
+	// 	fetchUserData()
+	// }, [])
+
+	return (
+		<ReactQueryProvider pageProps={pageProps}>
+			{getLayout(<Component {...pageProps} />)}
+		</ReactQueryProvider>
+	)
+}
+
+export default MyApp
---
+++ src/providers/react-query.tsx
@@ -0,0 +1,24 @@
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import {
+	Hydrate,
+	QueryClient,
+	QueryClientProvider,
+} from "@tanstack/react-query"
+import { ReactNode, useState } from "react"
+
+export const ReactQueryProvider = ({
+	children,
+	pageProps,
+}: {
+	children: ReactNode
+	pageProps: any
+}) => {
+	const [queryClient] = useState(() => new QueryClient({}))
+
+	return (
+		<QueryClientProvider client={queryClient}>
+			<Hydrate state={pageProps.dehydratedState}>{children}</Hydrate>
+			<ReactQueryDevtools initialIsOpen />
+		</QueryClientProvider>
+	)
+}
